079bfb62fe1f6840e88e64d1d46a392b94ab5271
Oliver-code1 <dubemoliver4@gmail.com>
2025-03-28 10:45:04 +0000

    Update mathmanagertest.py

--- mathmanagertest.py
+++ mathmanagertest.py
@@ -13,3 +13,17 @@ class mathmanagertest(unittest.TestCase):
 	def testMultiply(self):
 		math = mathmanager()
 		self.assertEqual(math.multiply(0, 3), 0)
+
+	def testInterest(self):
+		math= mathmanager()
+		self.assertEqual(math.interest (1000, 1), 3)
+
+	def testInterest1(self):
+		math= mathmanager()
+		self.assertEqual(math.interest (1000, 2), 3)
+
+	def testInterest2(self):
+		math= mathmanager()
+		self.assertEqual(math.interest (1000, 3), -2)
+
+#run test to show where it may fail, use all possible values for a particular function.
